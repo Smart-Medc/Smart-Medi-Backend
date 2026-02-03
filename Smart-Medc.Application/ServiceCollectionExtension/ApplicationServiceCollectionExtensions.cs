03fbdefbd431bd1499ec9f870523bc1288b6447b
AmarYasser <ssniperlove3@gmail.com>
2026-02-04 01:30:26 +0200

    Dependancy Injection and service setup for repositories

--- Smart-Medc.Application/ServiceCollectionExtension/ApplicationServiceCollectionExtensions.cs
+++ Smart-Medc.Application/ServiceCollectionExtension/ApplicationServiceCollectionExtensions.cs
@@ -6,7 +6,9 @@ namespace Smart_Medc.Application.ServiceCollectionExtension
     public static class ApplicationServiceCollectionExtensions
     {
         // This extension method is only used to register infrastructure services.
-        public static IServiceCollection AddApplicationServices(this IServiceCollection services, IConfiguration configuration)
+        public static IServiceCollection AddApplicationServices(
+            this IServiceCollection services,
+            IConfiguration configuration)
         {
             return services;
         }
