3b21aac7800e7f1855708a603116292d357dd7c7
AmarYasser <ssniperlove3@gmail.com>
2026-02-03 02:17:01 +0200

    ServiceCollectionExtension setup.

---
+++ Smart-Medc.Application/ServiceCollectionExtension/ApplicationServiceCollectionExtensions.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Smart_Medc.Application.ServiceCollectionExtension
+{
+    public static class ApplicationServiceCollectionExtensions
+    {
+        // This extension method is only used to register infrastructure services.
+        public static IServiceCollection AddApplicationServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            return services;
+        }
+    }
+}
---
+++ Smart_Medc.Infrastructure/ServiceCollectionExtension/InfrastructureServiceCollectionExtensions.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Smart_Medc.Infrastructure.ServiceCollectionExtension
+{
+    public static class InfrastructureServiceCollectionExtensions
+    {
+        // This extension method is only used to register infrastructure services.
+        public static IServiceCollection AddInfrastructureServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            return services;
+        }
+    }
+}
